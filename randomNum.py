8afc9ef45602070eba5c71d4d3c80ca3f96d55b5
firstblooidp <sergiu_andrei96@yahoo.com>
2017-10-31 13:56:58 +0200

    Changes

--- randomNum.py
+++ randomNum.py
@@ -2,20 +2,19 @@
 import random
 from datetime import datetime
 import matplotlib.pyplot as plt
+import numpy as np
 
 def lcg(n, m, a, c, seed):
 	numberList = []
 	duplicateCount = 0
-	Xn = int(seed)
+	Xn = float(seed)
 	for i in range(n):
-		Xn = int(Xn)
 		Xn = (a*Xn + c) % m
-		if len(str(int(Xn))) < 4:
-			Xn = ((4 - len(str(int(Xn)))) * '1') + str(Xn)
-			if int(Xn) in numberList:
-				duplicateCount += 1
-			numberList.append(int(Xn))
-	print duplicateCount 
+		if round(Xn % 1000) in numberList:
+			duplicateCount += 1
+		numberList.append(round(Xn%1000)) 
+	return numberList
+	#print duplicateCount 
 
 def midSquare(n,c,seed):
 	numberList = []
@@ -28,36 +27,147 @@ def midSquare(n,c,seed):
 		r = int(r)
 		if r % 25 == 0:
 			r += c
-		if len(str(r)) < 4:
-			r = str(r) + ((4 - len(str(int(r)))) * '0')
 		if r in numberList:
 			r = int(r) + c
-		if r in numberList:
+		if int(r) in numberList:
 			duplicateCount += 1
-		numberList.append(r)
-	print duplicateCount 
+		numberList.append(int(r))
+	return numberList
 
 #Also a good seed 
 #datetime.now().microsecond
-numberList = []
-duplicateCount = 0
+#numberList = []
+#duplicateCount = 0
+#x0 = []
+#y0 = []#
 
-for x in range(0,10000):
-	random1 = random.randint(1,9999)
-	if random1 in numberList:
-		duplicateCount += 1
-	numberList.append(random1)
+#for x in range(0,20000):
+#	random1 = random.randint(1,9999)
+#	#if random1 in numberList:
+#	#	duplicateCount += 1
+#	if x % 2 == 0:
+#		x0.append(random1)
+#	else:
+#		y0.append(random1)#
 
-print duplicateCount
-print
+#semicerc = 0
+#for i in range(10000):
+#	if (x0[i] ** 2) + (y0[i] ** 2) <= 100000000:
+#		semicerc += 1
+#print semicerc
+#print float(semicerc)/float(2500)
+#print#
 
-lcg(10000, 10000, 73, 89, 4693)
-print 
+#x1 = lcg(10000, 35371397137, 81, 19, 469314327.131197933)
+#y1 = lcg(10000, 35371397137, 81, 19, 347013719.177331533)#
 
-midSquare(10000,113,2500)   
-print
+#semicerc = 0
+#for i in range(10000):
+#	if (x1[i] ** 2) + (y1[i] ** 2) <= 100000000:
+#		semicerc += 1
+#print semicerc
+#print float(semicerc)/float(2500)
+#print#
 
-x = 5000
-y_2 = 5000
-plt.plot(x, y_2, "o", color="blue")
+#x2 = midSquare(10000,113,2500)   
+#y2 = midSquare(10000,113,1475)#
+
+#semicerc = 0
+#for i in range(10000):
+#	if (x2[i] ** 2) + (y2[i] ** 2) <= 100000000:
+#		semicerc += 1
+#print semicerc
+#print float(semicerc)/float(2500)
+#print#
+
+#cercx = [0]
+#cercy = [10000]
+
+#for i in range(10000):
+#	for j in range(10000):
+#		if (i ** 2) + (j ** 2) == 100000000:
+#			cercx.append(i)
+#			cercy.append(j)
+
+#cercx.append(10000)
+#cercy.append(0)
+
+#plt.plot(x0, y0, ".", color="green")
+#plt.plot(cercx,cercy, color="black")
+#plt.show()
+
+#plt.plot(x1, y1, ".", color="blue")
+#plt.plot(cercx,cercy, color="black")
+#plt.show()
+
+#plt.plot(x2, y2, ".", color="red")
+#plt.plot(cercx,cercy, color="black")
+
+aprVect = [0,]
+Vect = []
+
+for random_num in range(0,1000):
+	Vect.append(random.randint(1,1000))
+	aprVect.append(0)
+
+for part in Vect:
+	aprVect[part] += 1
+
+ox = []
+
+for nr in range (1,1001):
+	ox.append(nr)
+
+x = np.arange(1001)
+
+fig, ax = plt.subplots()
+plt.bar(x, aprVect)
+plt.xticks(x, ox)
+plt.show()
+
+
+aprVect = [0,]
+Vect = []
+
+for random_num in range(0,1000):
+	aprVect.append(0)
+
+Vect = lcg(10000, 35371397137, 81, 19, 469314327.131197933)
+
+for part in Vect:
+	aprVect[int(part)] += 1
+
+ox = []
+
+for nr in range (1,1001):
+	ox.append(nr)
+
+#x = np.arange(1000)
+
+fig, ax = plt.subplots()
+plt.bar(x, aprVect)
+plt.xticks(x, ox)
+plt.show()
+
+aprVect = [0,]
+Vect = []
+
+for random_num in range(0,1000):
+	aprVect.append(0)
+
+Vect = midSquare(1000,113,1475)
+
+for part in Vect:
+	aprVect[part%1001] += 1
+
+ox = []
+
+for nr in range (1,1001):
+	ox.append(nr)
+
+#x = np.arange(1000)
+
+fig, ax = plt.subplots()
+plt.bar(x, aprVect)
+plt.xticks(x, ox)
 plt.show()
